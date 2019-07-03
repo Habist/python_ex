3ce54b73cc7bba12ea0ceb6dbdac61dfe8ce3c82
LeeHanbin <liasn@naver.com>
2019-07-04 02:55:40 +0900

    WebCrawlerBySelenium.py - Selenium WebDriver Close 함수 추가

--- WebCrawlerBySelenium.py
+++ WebCrawlerBySelenium.py
@@ -7,6 +7,10 @@ from selenium.webdriver.common.by import By
 
 driver_path = "C:\\tensorflow1\\chromedriver.exe"
 
-ff_driver = webdriver.Chrome(driver_path)
+chr_driver = webdriver.Chrome(driver_path)
 
-ff_driver.get("https://www.google.co.kr")
+chr_driver.get("https://www.google.co.kr")
+
+
+chr_driver.close()
+chr_driver.quit()
