2bb73c419e97030b3463a0b0e6ccaeda03619ea4
LeeHanbin <liasn@naver.com>
2019-07-06 01:53:30 +0900

    WebCrawlerBySelenium.py - Selenium class 요소 text 가져오는 함수 추가

--- WebCrawlerBySelenium.py
+++ WebCrawlerBySelenium.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+def getText(drive: webdriver.chrome, name: str) :
+    return drive.find_element_by_class_name(name).text
 
 driver_path = "C:\\tensorflow1\\chromedriver.exe"
 
@@ -14,12 +16,17 @@ chr_driver.find_element_by_id("search_btn").click()
 
 WebDriverWait(chr_driver, 100).until(EC.presence_of_all_elements_located)
 today = chr_driver.find_elements_by_class_name("menu")[4].text
-ment = today + "의 '" +chr_driver.find_element_by_class_name("btn_select").text + "' 기온은 " + chr_driver.find_element_by_class_name("todaytemp").text +"도 입니다."
+
+ment = today + "의 '" + getText(chr_driver, "btn_select") + "' 기온은 " + getText(chr_driver,"todaytemp") + "도 입니다."
 print(ment)
 
-#for idx, val in enumerate(elements):
+
+# for idx, val in enumerate(elements):
 #    print(idx)
 #    print(val.text)
 
-#chr_driver.close()
-#chr_driver.quit()
+# chr_driver.close()
+# chr_driver.quit()
+
+
+
