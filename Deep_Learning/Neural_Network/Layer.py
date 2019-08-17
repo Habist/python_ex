196a1a29e4626bd08786f071d09c505cb0a552fc
LeeHanbin <liasn@naver.com>
2019-08-17 17:49:33 +0900

    Deep_Learning - Overfitting 억제  - DropOut 클래스 구현

--- Deep_Learning/Neural_Network/Layer.py
+++ Deep_Learning/Neural_Network/Layer.py
@@ -111,4 +111,20 @@ class SoftmaxWithLoss:
         dx = (self.y - self.t) / batch_size
 
         return dx
-    
+
+
+class DropOut:
+    def __init__(self, dropout_ratio = 0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+
+
+    def backward(self, dout):
+        return dout * self.mask
