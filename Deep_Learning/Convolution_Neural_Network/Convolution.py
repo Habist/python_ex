aed5fefe9140a84d2cc0cdb2314f38efe9f5d200
LeeHanbin <liasn@naver.com>
2019-09-07 20:39:37 +0900

    CNN - Convolution 계층 역전파 함수 추가

--- Deep_Learning/Convolution_Neural_Network/Convolution.py
+++ Deep_Learning/Convolution_Neural_Network/Convolution.py
@@ -8,6 +8,13 @@ class Convolution:
         self.stride = stride
         self.pad = pad
 
+        self.x = None
+        self.col = None
+        self.col_W = None
+
+        self.dW = None
+        self.db = None
+
     def forward(self, x):
         FN, C, FH, FW = self.W.shape
         N, C, H, W = x.shape
@@ -15,9 +22,26 @@ class Convolution:
         out_w = int(1 + (W + 2*self.pad - FW) / self.stride)
 
         col = im2col(x, FH, FW, self.stride, self.pad)
-        col_W = self.W.reshape(FN, -1).T
-        out = np.dot(col, col_W) + self.b
+        col_W = self.W.reshape(FN, -1).T    # FN개의 필터를 평탄화 한후에 전치행렬로 변환 후 행렬 곱 = 합성곱
+        out = np.dot(col, col_W) + self.b   # 합성곱에서는 필터가 가중치 역할을 함
 
-        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)    # 기존 형태로 다시 변형
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
 
         return out
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN) # 변형된 형태 이전으로 다시 변형
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
