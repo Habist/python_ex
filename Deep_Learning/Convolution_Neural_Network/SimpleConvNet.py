5fa8a1d092b3284312348f24b09d42e5c8781ef3
LeeHanbin <liasn@naver.com>
2019-09-10 23:45:39 +0900

    CNN - SimpleConvNet 초기 변수명 오타 수정 및 역전파 추가

--- Deep_Learning/Convolution_Neural_Network/SimpleConvNet.py
+++ Deep_Learning/Convolution_Neural_Network/SimpleConvNet.py
@@ -7,7 +7,7 @@ from Layer import *
 class SimpleConvNet:
     def __init__(self, input_dim=(1, 28, 28),
                  conv_param={'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
-                 hidden_size = 100, output_size = 10, weigth_init_std = 0.01):
+                 hidden_size = 100, output_size = 10, weight_init_std = 0.01):
         filter_num = conv_param['filter_num']
         filter_size = conv_param['filter_size']
         filter_pad = conv_param['pad']
@@ -18,19 +18,19 @@ class SimpleConvNet:
         pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
 
         self.params = {}
-        self.params['W1'] = weigth_init_std * np.random.randn(filter_num, input_dim[0],
+        self.params['W1'] = weight_init_std * np.random.randn(filter_num, input_dim[0],
                                                               filter_size, filter_size)
         self.params['b1'] = np.zeros(filter_num)
-        self.params['W2'] = weigth_init_std * np.random.randn(pool_output_size, hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(pool_output_size, hidden_size)
         self.params['b2'] = np.zeros(hidden_size)
-        self.params['W3'] = weigth_init_std * np.random.randn(hidden_size, output_size)
+        self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b3'] = np.zeros(output_size)
 
         self.layers = OrderedDict()
         self.layers['Conv1'] = Convolution(self.params['W1'],
                                            self.params['b1'],
-                                           self.params['stride'],
-                                           self.params['pad'])
+                                           conv_param['stride'],
+                                           conv_param['pad'])
         self.layers['Relu1'] = Relu()
         self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
         self.layers['Affine1'] = Affine(self.params['W2'],
@@ -39,6 +39,42 @@ class SimpleConvNet:
         self.layers['Affine2'] = Affine(self.params['W3'],
                                         self.params['b3'])
         self.last_layer = SoftmaxWithLoss()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.last_layer.forward(y, t)
+
+    def gradient(self, x, t):
+        # 순전파
+        self.loss(x,t)
+
+        # 역전파
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+
+        # 결과 저장
+        grads = {}
+        grads['W1'] = self.layers['Conv1'].dW
+        grads['b1'] = self.layers['Conv1'].db
+        grads['W2'] = self.layers['Affine1'].dW
+        grads['b2'] = self.layers['Affine1'].db
+        grads['W3'] = self.layers['Affine2'].dW
+        grads['b3'] = self.layers['Affine2'].db
+
+        return grads
+
+
 
 
 
