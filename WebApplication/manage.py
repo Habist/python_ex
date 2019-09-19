0555683a8524dbea2d94af4c22f8efc3515f7ae3
LeeHanbin <liasn@naver.com>
2019-09-20 01:06:05 +0900

    Django - view, urls 기본 설정

--- WebApplication/manage.py
+++ WebApplication/manage.py
@@ -2,15 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import pickle
+
 
 def main():
-    # train_instance = dl.load_instance('train_class')
-
-    with open("../Deep_Learning/Neural_Network/saveNetwork/train_class"+".pkl","rb") as file:
-        save_instance = pickle.load(file)
-    print(save_instance.__class__.__name__)
-    print("load success")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HabiWeb.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -22,7 +16,6 @@ def main():
 
         ) from exc
     execute_from_command_line(sys.argv)
-    # train_instance.train_step()
 
 
 if __name__ == '__main__':
