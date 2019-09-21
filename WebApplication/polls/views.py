077b6c32da16f3df23f102520d0e492904326bd0
LeeHanbin <liasn@naver.com>
2019-09-22 01:03:37 +0900

    Django - views <--> models 연결, 템플릿 시스템 사용, urls 매핑 추가

--- WebApplication/polls/views.py
+++ WebApplication/polls/views.py
@@ -1,19 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index!!")
-
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+    ### 템플릿에 context를 채워넣어 표현한 결과를 HttpRespose와 같이 반환하는 형태를 단축 기능을 통해 사용가능
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def test(request):
     return HttpResponse("test!!")
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+    ### 단축 기능 get_object_or_404
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
